4611c0727fa711f0e8ca008b0f23d43c1611b4a5
Santiago Giraldo <santigibo@hotmail.com>
2020-02-19 11:56:05 -0500

    Creted the rails files, the models restaurant and review, with validateions and connections between the db tables. rspec all green

---
+++ app/models/restaurant.rb
@@ -0,0 +1,7 @@
+class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+
+  a = %w[chinese italian japanese french belgian]
+  validates :category, inclusion: { in: a, meesage: 'Not available category' }
+  validates :name, :address, :phone_number, presence: true
+end
